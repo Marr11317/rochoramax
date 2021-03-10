3732f1375220a18386eddb678ed553c728c345e9
Marr11317 <remimarche@gmail.com>
2021-03-09 22:09:57 -0500

    init pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { BbGoogleMaps } from "./bb-google-maps";
-import { BbInfoWindow } from "./bb-info-window";
-import { BbMarkers } from "./bb-markers";
-
-BbGoogleMaps.load(function () {
-    BbGoogleMaps.displayMap();
-    BbInfoWindow.infoWindow = new google.maps.InfoWindow();
-    BbMarkers.loadSheet(BbGoogleMaps.map);
-});
